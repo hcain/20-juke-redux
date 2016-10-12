c93dbbe9eb535ad67aba5ac7d52546a53858fbfe
mithunder <mselvaratnam@gmail.com>
2016-10-12 15:13:41 -0400

    added comments

--- browser/react/myRedux.js
+++ browser/react/myRedux.js
@@ -3,9 +3,11 @@ import initialState from './initialState';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
+// creating store and passing it middleware for logging and treating action creators as functions
 const logger = createLogger();
 const Store = createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
+// helper functions for dealing with albums
 const convertSong = song => {
   song.audioUrl = `/api/songs/${song.id}/audio`;
   return song;
@@ -19,11 +21,13 @@ const convertAlbum = album => {
 
 const RECEIVE_ALBUMS_FROM_SERVER = 'RECEIVE_ALBUMS_FROM_SERVER';
 
+// action creator that is dispatched and gets albums
 const receiveAlbums = (albums) =>
   ({ type: RECEIVE_ALBUMS_FROM_SERVER,
      albums }
 );
 
+// action creator that fetches albums from server and calls receiveAlbums
 const fetchAlbumsFromServer = () => {
   return (dispatch) => {
     fetch('api/albums')
@@ -33,7 +37,7 @@ const fetchAlbumsFromServer = () => {
   }
 }
 
-
+// reducer function
 function reducer (state = initialState, action) {
   switch (action.type) {
     case RECEIVE_ALBUMS_FROM_SERVER:
@@ -43,7 +47,6 @@ function reducer (state = initialState, action) {
 }
 
 Store.getState();
-// Store.dispatch(receiveAlbums);
 Store.getState();
 
 export { Store, receiveAlbums, fetchAlbumsFromServer };
